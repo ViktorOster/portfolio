203dd100ef08b644beb073fd17b5ccfa23277fca
ViktorOster <Ozterkvlt@hotmail.com>
2019-02-21 02:46:48 +0200

    Updates

---
+++ precache-manifest.282be326e1fa79a719d54eba585ac162.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "140e553e5a048399c01eef400497d467",
+    "url": "/portfolio/static/media/MedicalDashboard.140e553e.png"
+  },
+  {
+    "revision": "1a9493bc14b89d85f9af",
+    "url": "/portfolio/static/css/main.777c4650.chunk.css"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "c0601c14529b5ede0879",
+    "url": "/portfolio/static/js/2.c0601c14.chunk.js"
+  },
+  {
+    "revision": "c586a0fea38d67dfd7c492980c1a927d",
+    "url": "/portfolio/static/media/hero-image-transparent.c586a0fe.png"
+  },
+  {
+    "revision": "4691e0456292ecdaa392062bcce78f15",
+    "url": "/portfolio/static/media/SPMS.4691e045.png"
+  },
+  {
+    "revision": "1a9493bc14b89d85f9af",
+    "url": "/portfolio/static/js/main.1a9493bc.chunk.js"
+  },
+  {
+    "revision": "0893ba4ff72bd36a4f1f7962666e7e17",
+    "url": "/portfolio/static/media/eAuctionDjango.0893ba4f.png"
+  },
+  {
+    "revision": "9f2486bd0d69ea4a7e8d50236c1167b8",
+    "url": "/portfolio/static/media/DrawWaveSynth.9f2486bd.png"
+  },
+  {
+    "revision": "8c11839c8deda3a1453f7db2ac318665",
+    "url": "/portfolio/static/media/SketchBoardgallery.8c11839c.png"
+  },
+  {
+    "revision": "e77259b923a65c0d83467a90f098225b",
+    "url": "/portfolio/static/media/PortfolioPage.e77259b9.png"
+  },
+  {
+    "revision": "694bfb3d702cd0298a248d01b86c3bdf",
+    "url": "/portfolio/static/media/Jag.694bfb3d.jpg"
+  },
+  {
+    "revision": "d8572d8b4a00ce752f6d81c2fb30c3f9",
+    "url": "/portfolio/index.html"
+  }
+];
